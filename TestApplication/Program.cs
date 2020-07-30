e2e57c808861bdf122db8fb37ae6ec5eba5d6d98
petrprog1997 <55106133+petrprog1997@users.noreply.github.com>
2020-07-30 15:05:39 +0300

    Add files via upload

---
+++ TestApplication/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestApplication
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //application.enablevisualstyles();
+            //application.setcompatibletextrenderingdefault(false);
+            //application.run(new form1());
+            string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            Console.WriteLine(connectionString);
+
+            Console.Read();
+
+
+        }
+    }
+}
